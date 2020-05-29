61243ef766c66273903139a3d732dd1654079dcf
Hyun Min Choi <hyunmin.personal@gmail.com>
2020-05-29 15:36:28 +0900

    Change training model

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import * as d3 from 'd3'
-import { bootstrap } from './nnbootstrap';
 import { ripple, slider } from 'material-components-web'
+
+import { bootstrap } from './nnbootstrap';
 
 // const { ripple, slider } = mdc;
 const { MDCSlider } = slider;
@@ -16,9 +17,6 @@ const getEpoch = () => epochSlider.value;
 
 const width = 5000;
 const height = 5000;
-
-console.log("Bootstrapping");
-bootstrap();
 
 const cnn = {
   conv: [ 
@@ -60,62 +58,76 @@ const cnn = {
   ]
 };
 
-const svg = d3.select("body")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height);
+function visualize(model) {
+  const svg = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-// Conv Layers
-const conv = svg.selectAll(".rect")
-  .data(cnn.conv)
-  .enter()
-  .append("g")
-  .classed('rect', true);
+  // Conv Layers
+  const conv = svg.selectAll(".rect")
+    .data(cnn.conv)
+    .enter()
+    .append("g")
+    .classed('rect', true);
 
-conv
-  .append("text")
-  .text("Convolution Layers");
+  conv
+    .append("text")
+    .text("Convolution Layers");
 
-const colorScale = d3.scaleLinear().domain([0.0, 1.0]).range(['white', 'black']);
+  const colorScale = d3.scaleLinear().domain([0.0, 1.0]).range(['white', 'black']);
 
-cnn.conv.forEach((layer, idx) => {
-  const layerAgg = conv.append("g")
-    .attr("transform", d => "translate(" + (idx * 80.0) + "," + 0.0 + ")")
-    .attr("stroke", "black");
+  cnn.conv.forEach((layer, idx) => {
+    const layerAgg = conv.append("g")
+      .attr("transform", d => "translate(" + (idx * 80.0) + "," + 0.0 + ")")
+      .attr("stroke", "black");
 
-  for (var row = 0; row < layer.weights.length; row++) {
-    for (var col = 0; col < layer.weights[row].length; col++) {
-      layerAgg.append("rect")
-        .attr("width", 20)
-        .attr("height", 20)
-        .attr("x", row * 20)
-        .attr("y", col * 20)
-        .attr("fill", d => colorScale(layer.weights[row][col]));
+    for (var row = 0; row < layer.weights.length; row++) {
+      for (var col = 0; col < layer.weights[row].length; col++) {
+        layerAgg.append("rect")
+          .attr("width", 20)
+          .attr("height", 20)
+          .attr("x", row * 20)
+          .attr("y", col * 20)
+          .attr("fill", d => colorScale(layer.weights[row][col]));
+      }
     }
-  }
-});
+  });
 
-const subsampling = svg.selectAll(".subsampling")
-  .data(cnn.subsampling)
-  .enter()
-  .append("g");
+  const subsampling = svg.selectAll(".subsampling")
+    .data(cnn.subsampling)
+    .enter()
+    .append("g");
 
-cnn.subsampling.forEach((layer, idx) => {
-  const layerAgg = subsampling.append("g")
-    .attr("transform", d => "translate(" + (idx * 60.0) + "," + 0.0 + ")")
-    .attr("stroke", "black");
+  cnn.subsampling.forEach((layer, idx) => {
+    const layerAgg = subsampling.append("g")
+      .attr("transform", d => "translate(" + (idx * 60.0) + "," + 0.0 + ")")
+      .attr("stroke", "black");
 
-  for (var row = 0; row < layer.weights.length; row++) {
-    for (var col = 0; col < layer.weights[row].length; col++) {
-      layerAgg.append("rect")
-        .attr("width", 20)
-        .attr("height", 20)
-        .attr("x", row * 20)
-        .attr("y", col * 20)
-        .attr("fill", d => colorScale(layer.weights[row][col]));
+    for (let row = 0; row < layer.weights.length; row++) {
+      for (let col = 0; col < layer.weights[row].length; col++) {
+        layerAgg.append("rect")
+          .attr("width", 20)
+          .attr("height", 20)
+          .attr("x", row * 20)
+          .attr("y", col * 20)
+          .attr("fill", d => colorScale(layer.weights[row][col]));
+      }
     }
-  }
-});
+  });
 
-subsampling
-  .attr("transform", d => "translate(" + 0 + "," + 100 + ")");
+  subsampling
+    .attr("transform", d => "translate(" + 0 + "," + 100 + ")");
+}
+
+function init() {
+  console.log('Bootstrapping');
+
+  bootstrap()
+    .then(model => {
+      console.log('Bootstrapping finished.');
+      visualize(model);
+    })
+}
+
+init();
