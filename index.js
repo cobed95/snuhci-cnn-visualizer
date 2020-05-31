bdcae279bde67345656ae0b108f6a288053352ab
Eundo Lee <cobed95@gmail.com>
2020-06-01 01:55:36 +0900

    load screen implemented

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ import { bootstrap, getRawData } from './nnbootstrap';
 import Visualizer from './Visualizer';
 import ModelController from './ModelController';
 
+const hideLoading = () => {
+  document.getElementById("loading").style.display = "none";
+}
+
+const showCnnVis = () => {
+  document.getElementById("cnn-visualizer").style.display = "block";
+}
+
 function init() {
   console.log('Bootstrapping');
 
@@ -11,6 +19,8 @@ function init() {
       const data = getRawData();
       const visualizer = new Visualizer(model, data);
       const modelController = new ModelController(model, data, visualizer);
+      hideLoading();
+      showCnnVis();
     });
 }
 
